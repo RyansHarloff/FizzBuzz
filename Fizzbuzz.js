5f8a36c281d1e70a44f1133741e5afe22c74d26b
Ryan Harloff <ryans.harloff@gmail.com>
2021-04-23 14:48:30 -0500

    w

--- Fizzbuzz.js
+++ Fizzbuzz.js
@@ -1,6 +1,6 @@
-for(var i=1; i < 101; i ++){
-    console.log(i);
-}
+// for(var i=1; i < 101; i ++){
+//     console.log(i);
+// }
 
 for (var i=1; i < 101; i++){
     if (i % 15 == 0) console.log("FizzBuzz");
